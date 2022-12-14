a6c1ddb0baa44f304bf11ab05dadfbd3cea71417
caroline cloughley <caro672g@stud.kea.dk>
2022-12-15 00:44:19 +0100

    acts list alphabetical

--- src/components/ActList.jsx
+++ src/components/ActList.jsx
@@ -3,15 +3,41 @@ import ActListCard from "./ActListCard";
 import { Link } from "react-router-dom";
 
 function ActList(props) {
+  const slots = props.slots;
+  let bands = props.bands;
+  bands.sort((a, b) => {
+    if (a.name > b.name) {
+      return 1;
+    }
+    if (a.name < b.name) {
+      return -1;
+    }
+    return 0;
+  });
+
+  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+
   return (
     <div className="actList">
       <h1>This years Acts</h1>
       <div className="listOfActs">
-        {props.bands.map((band) => (
-          <Link to={`/acts/${band.name}`} key={band.name}>
-            <ActListCard key={band.name} data={band} slot={props.slots[band.name]} />{" "}
-          </Link>
-        ))}
+        {alphabet.map((letter) => {
+          return (
+            <div>
+              <h1>{letter}</h1>
+              {bands.map((band) => {
+                if (band.name.charAt(0) === letter)
+                  return (
+                    <div>
+                      <Link to={`/acts/${band.name}`} key={band.name}>
+                        <ActListCard key={band.name} data={band} slot={slots[band.name]} />{" "}
+                      </Link>
+                    </div>
+                  );
+              })}
+            </div>
+          );
+        })}
       </div>
     </div>
   );
