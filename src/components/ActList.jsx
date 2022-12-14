91e61404547e853ab1f708773f98255224c356eb
caroline cloughley <caro672g@stud.kea.dk>
2022-12-14 19:50:18 +0100

    end of wednesday styling

--- src/components/ActList.jsx
+++ src/components/ActList.jsx
@@ -4,13 +4,15 @@ import { Link } from "react-router-dom";
 
 function ActList(props) {
   return (
-    <div className="Actlist">
-      <h1>All bands Playing Tonal Festival!</h1>
-      {props.bands.map((band) => (
-        <Link to={`/acts/${band.name}`} key={band.name}>
-          <ActListCard key={band.name} data={band} slot={props.slots[band.name]} />{" "}
-        </Link>
-      ))}
+    <div className="actList">
+      <h1>This years Acts</h1>
+      <div className="listOfActs">
+        {props.bands.map((band) => (
+          <Link to={`/acts/${band.name}`} key={band.name}>
+            <ActListCard key={band.name} data={band} slot={props.slots[band.name]} />{" "}
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
